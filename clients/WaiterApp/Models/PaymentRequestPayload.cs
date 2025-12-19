b4101109fa7a5dcb1af163a47aab79a10b5eadc4
quypq147 <phanngocquy1472004@gmail.com>
2025-12-19 12:15:38 +0700

    [dev] update

--- clients/WaiterApp/Models/PaymentRequestPayload.cs
+++ clients/WaiterApp/Models/PaymentRequestPayload.cs
@@ -5,5 +5,7 @@ namespace WaiterApp.Models;
 public sealed class PaymentRequestPayload
 {
     public Guid OrderId { get; set; }
+
+    public Guid TableId { get; set; }
     public string TableCode { get; set; } = string.Empty;
 }
